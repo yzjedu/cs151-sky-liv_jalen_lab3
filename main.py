050bfd3babea2311a9034b6db9578e335f6ec75f
Jalen Henderson <jkhenderson@loyola.edu>
2024-10-02 06:24:16 -0400

    Updated test cases and main program

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#This program will determine the distance traveled and total points earned for a ski jump given the speed and hill type.
+#This program will determine the distance traveled and total points earned
+# for a ski jump given the speed and hill type.
+import math
 
-#
 hill_type = input("Is your hill type normal or large?")
 
 #After determining the hill type, the constants for height, points per meter, and par,
@@ -15,4 +16,23 @@ elif hill_type == "large":
     points_per_meter = 1.8
     par = 120
 else:
-    print("Invalid hill type, try again.")
+    print("Invalid hill type, start over.")
+
+#This line prompts the user for their speed
+jumper_speed = float(input("What was your speed at the end of the ramp?"))
+
+#These 3 lines of code do the necessary calculations to find the users distance and earned points
+air_time = math.sqrt((2 * height)/9.8)
+distance_traveled = jumper_speed * air_time
+earned_points = 60 + (distance_traveled - par) * points_per_meter
+
+#Lastly, these lines output the distance, points, and a different message
+# depending on how many points the user scored
+print("Distance:", distance_traveled)
+print("Earned points:", earned_points)
+if earned_points >= 61:
+    print("Great job for doing better than par!")
+elif earned_points < 10:
+    print("What happened?")
+else:
+    print("Sorry you didn't go very far.")
